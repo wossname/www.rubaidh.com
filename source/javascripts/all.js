66ff079b6b1d20ad2ae53689971741b4339d1d6d
Graeme Mathieson <mathie@woss.name>
2016-05-11 10:37:53 +0100

    Initial import of the Unify coming soon page theme.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,12 @@
-// This is where it all goes :)
+jQuery(document).ready(function() {
+  App.init();
+});
+
+$.backstretch([
+  "/images/14.jpg",
+  "/images/17.jpg",
+  "/images/2.jpg"
+], {
+  fade: 1000,
+  duration: 7000
+});
