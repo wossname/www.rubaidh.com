5b60e09322f6ee961bcafd05d0c80bf557bce023
Graeme Mathieson <mathie@woss.name>
2016-05-15 06:00:54 +0100

    Shuffle the config.rb around so it's consistent with other sites.

--- config.rb
+++ config.rb
@@ -1,30 +1,22 @@
-###
-# Page options, layouts, aliases and proxies
-###
-
-config[:domain] = 'rubaidh.com'
-config[:hostname] = "www.#{config[:domain]}"
-config[:url] = "https://#{config[:hostname]}"
+# General configuration
+config[:domain]                  = 'rubaidh.com'
+config[:www_prefix]              = true
 config[:cloudfront_distribution] = 'E1STMQ3FOGJ7AN'
 
 # UTM-related bits
-config[:default_utm_source] = config[:domain]
-config[:default_utm_medium] = 'website'
+config[:default_utm_medium]   = 'website'
 config[:default_utm_campaign] = 'Rubaidh'
 
-# Per-page layout changes:
-#
-# With no layout
+# Calculated Configuration
+config[:hostname]           = config[:www_prefix] ? "www.#{config[:domain]}" : config[:domain]
+config[:url]                = "https://#{config[:hostname]}"
+config[:email_address]      = "hello@#{config[:domain]}"
+config[:default_utm_source] = config[:domain]
+
+# Pages with no layout
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
-
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
 
 # General configuration
 activate :directory_indexes
@@ -41,12 +33,35 @@ configure :development do
   activate :livereload
 end
 
-###
-# Helpers
-###
+# Build-specific configuration
+configure :build do
+  activate :minify_html
+end
 
-# Methods defined in the helpers block are available in templates
+activate :s3_sync do |s3|
+  s3.bucket = config[:hostname]
+  s3.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
+  s3.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
+end
+
+activate :cloudfront do |cf|
+  cf.access_key_id = ENV['AWS_ACCESS_KEY_ID']
+  cf.secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
+  cf.distribution_id = config[:cloudfront_distribution]
+  cf.filter = /\.(html|xml|txt)$/
+end
+
+caching_policy 'text/html',       max_age: 0, must_revalidate: true
+caching_policy 'application/xml', max_age: 0, must_revalidate: true
+caching_policy 'text/plain',      max_age: 0, must_revalidate: true
+
+default_caching_policy max_age: (60 * 60 * 24 * 365)
+
 helpers do
+  def mail_to_link(options = {})
+    mail_to config[:email_address], config[:email_address], options
+  end
+
   def utm_link_to(title_or_url, url_or_options = nil, options = {}, &block)
     if block_given?
       title   = nil
@@ -78,28 +93,9 @@ helpers do
       link_to title, url, options
     end
   end
-end
 
-# Build-specific configuration
-configure :build do
-  # Minify ALL THE THINGS.
-  # activate :minify_css
-  # activate :minify_javascript
-  activate :minify_html
+  def xml_timestamp(dateish = Time.now)
+    timestamp = dateish.respond_to?(:strftime) ? dateish : Date.parse(dateish)
+    timestamp.strftime('%FT%H:%M:%S%:z') 
+  end
 end
-
-activate :s3_sync do |s3|
-  s3.bucket = config[:hostname]
-  s3.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
-  s3.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
-end
-
-activate :cloudfront do |cf|
-  cf.access_key_id = ENV['AWS_ACCESS_KEY_ID']
-  cf.secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
-  cf.distribution_id = config[:cloudfront_distribution]
-  cf.filter = /\.(html|xml)$/
-end
-
-caching_policy 'text/html', max_age: 0, must_revalidate: true
-default_caching_policy max_age: (60 * 60 * 24 * 365)
